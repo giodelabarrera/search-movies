55fa96536f60f1ccbb8a461936a8c7bb34b04d6c
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-03-27 16:34:13 +0200

    search and results to apply style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Title} from './components/Title';
+import {SearchForm} from "./components/SearchForm";
+
 import './App.css';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
+  
+  state = {
+    results: []
+  };
+  
+  _handleResults = (results) => {
+    console.log(results);
+    this.setState({ results });
+  };
+  
+  _renderResults() {
+    const {results} = this.state;
+    //console.log(results);
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>;
+    });
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="section has-text-centered">
+        <div className="container">
+          <Title>Search Movies</Title>
+          <div className="SearchForm-wrapper">
+            <SearchForm onResults={this._handleResults}/>
+          </div>
+          {this.state.results.length === 0
+          ? <p className="">Sin resultados</p>
+          : this._renderResults()}
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+const API_KEY = '4287ad07';
+
+export class SearchForm extends Component {
+  
+  state = {
+    inputMovie: ''
+  };
+  
+  _handleChange = (e) => {
+    this.setState({inputMovie: e.target.value});
+    //console.log(this.state.inputMovie);
+  };
+  
+  _handleSubmit = (e) => {
+    e.preventDefault();
+    //alert(`Your movie to search is: ${this.state.inputMovie}`);
+    const {inputMovie} = this.state;
+  
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(res => res.json())
+      .then(results => {
+        const { Search, totalResults } = results;
+        console.log(Search, totalResults);
+        this.props.onResults(Search);
+      })
+    ;
+    
+  };
+  
+  render() {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="Movie to search..."
+              onChange={this._handleChange}
+            />
+          </div>
+          <div className="control">
+            <button className="button is-info">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
